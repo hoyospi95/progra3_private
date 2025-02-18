0ad0d796632baec58dcd47d4701fdb400af6ec42
hoyospi95 <jorge.hoyos@uptc.edu.co>
2025-02-18 10:43:35 -0500

    subir proyecto

---
+++ pruebas/src/pruebas/Test.java
@@ -0,0 +1,17 @@
+package pruebas;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
+
+public class Test {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		
+	}
+
+}
